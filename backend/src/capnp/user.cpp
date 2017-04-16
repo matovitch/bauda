dd77691f6f50a4ea3fa063d9acbe19fa61a389a6
Camille Brugel <camille.brugel@openmailbox.org>
2017-04-16 12:25:12 +0200

    tupfile cleaning

---
+++ backend/src/capnp/user.cpp
@@ -0,0 +1,36 @@
+#include "user.hpp"
+
+#include "../../capnp/gen/user.capnp.h"
+#include <capnp/serialize-packed.h>
+#include <capnp/message.h>
+
+#include "utils/scrypt.hpp"
+#include "utils/capnp.hpp"
+
+#include <kj/io.h>
+
+#include <string>
+
+namespace user
+{
+
+std::string create(const std::string& name,
+                   const std::string& password,
+                   const std::string& email)
+{
+    capnp::MallocMessageBuilder message;
+    User::Builder user = message.initRoot<User>();
+
+    user.setName (name);
+    user.setEmail(email);
+
+    scrypt_utils::HashAndSalt hashAndSalt = scrypt_utils::craftHashAndSalt(password);
+
+    user.getPassword().setHash(capnp::Data::Reader(hashAndSalt.hash.data(), 
+                                                   hashAndSalt.hash.size()));
+    user.getPassword().setSalt(hashAndSalt.salt);
+
+    return capnp_utils::stringFromMessage(message);
+}
+
+} // end user namespace
--- backend/src/capnp/user.hpp
+++ backend/src/capnp/user.hpp
@@ -1,15 +1,6 @@
 #ifndef __CAPNP_USER_H__
 #define __CAPNP_USER_H__
 
-#include "../../capnp/gen/user.capnp.h"
-#include <capnp/serialize-packed.h>
-#include <capnp/message.h>
-
-#include "utils/scrypt.hpp"
-#include "utils/capnp.hpp"
-
-#include <kj/io.h>
-
 #include <string>
 
 namespace user
@@ -17,22 +8,7 @@ namespace user
 
 std::string create(const std::string& name,
                    const std::string& password,
-                   const std::string& email)
-{
-    capnp::MallocMessageBuilder message;
-    User::Builder user = message.initRoot<User>();
-
-    user.setName (name);
-    user.setEmail(email);
-
-    scrypt_utils::HashAndSalt hashAndSalt = scrypt_utils::craftHashAndSalt(password);
-
-    user.getPassword().setHash(capnp::Data::Reader(hashAndSalt.hash.data(), 
-                                                   hashAndSalt.hash.size()));
-    user.getPassword().setSalt(hashAndSalt.salt);
-
-    return capnp_utils::stringFromMessage(message);
-}
+                   const std::string& email);
 
 } // end user namespace
 
